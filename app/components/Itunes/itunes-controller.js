6f8c58f86e159c64af1ce228e605a82390378dd0
Clair Rogers <clair-rogers@outlook.com>
2019-02-15 16:21:41 -0700

    progress

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -6,14 +6,20 @@ function drawSongs() {
   //changes button back to GET MUSIC once songs are loaded
   document.querySelector('#get-music-button').textContent = 'GET MUSIC'
   console.log(itunesService.Songs)
-
+  let songs = itunesService.Songs
+  let template = ''
+  songs.forEach(song => {
+    template += song.getTemplate()
+  });
+  document.getElementById('songs').innerHTML = template
 }
+
 
 
 //PUBLIC
 class ItunesController {
   constructor() {
-    //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+    itunesService.addSubscriber('songs', drawSongs)
   }
 
 
@@ -24,7 +30,13 @@ class ItunesController {
     //changes the button to loading while songs load
     document.querySelector('#get-music-button').textContent = 'LOADING....'
     itunesService.getMusicByArtist(artist)
+    document.getElementById('message').style.opacity = '1'
+    document.getElementById('footer').style.opacity = '1'
   }
+  playSong(url) {
+    document.querySelector('source').src = url
+  }
+
 }
 
 
--- app/models/Song.js
+++ app/models/Song.js
@@ -8,4 +8,20 @@ export default class Song {
         this.price = song.collectionPrice
         this.preview = song.previewUrl
     }
+    getTemplate() {
+        return `
+        <div class="col-12 col-md-3 py-2">
+                <div class="card">
+                    <img src="${this.albumArt}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4>${this.artist}</h4>
+                        <h5>${this.title}</h5>
+                        <p>${this.collection}<p>
+                        <h5><b>$${this.price}</b></h5>
+                        <button type="button" class="btn btn-warning w-100" onclick="app.controllers.itunesCtrl.playSong(${this.preview})">Play</button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
 }
